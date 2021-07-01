d853f1ba720c914cb85699a0ca7e02199625076a
Pavel Voropaev <voropaev.p@gmail.com>
2021-07-01 01:47:05 +0100

    cats-effect 3 and updating for the PR2219

--- quill-ce/src/main/scala-2.12/io/getquill/context/ce/CeContext.scala
+++ quill-ce/src/main/scala-2.12/io/getquill/context/ce/CeContext.scala
@@ -1,16 +1,15 @@
 package io.getquill.context.ce
 
 import io.getquill.NamingStrategy
-import io.getquill.context.{ Context, StreamingContext }
+import io.getquill.context.{ Context, ExecutionInfo, StreamingContext }
+import fs2.{ Stream => FStream }
 import scala.language.higherKinds
 
 trait CeContext[Idiom <: io.getquill.idiom.Idiom, Naming <: NamingStrategy, F[_]]
   extends Context[Idiom, Naming]
   with StreamingContext[Idiom, Naming] {
 
-  type Error
-  type Environment
-
+  override type StreamResult[T] = FStream[F, T]
   override type Result[T] = F[T]
   override type RunActionResult = Unit
   override type RunBatchActionResult = Unit
@@ -18,7 +17,7 @@ trait CeContext[Idiom <: io.getquill.idiom.Idiom, Naming <: NamingStrategy, F[_]
   override type RunQuerySingleResult[T] = T
 
   // Need explicit return-type annotations due to scala/bug#8356. Otherwise macro system will not understand Result[Long]=Task[Long] etc...
-  def executeQuery[T](sql: String, prepare: Prepare = identityPrepare, extractor: Extractor[T] = identityExtractor): Result[RunQueryResult[T]]
-
-  def executeQuerySingle[T](sql: String, prepare: Prepare = identityPrepare, extractor: Extractor[T] = identityExtractor): Result[RunQuerySingleResult[T]]
+//  def executeQuery[T](sql: String, prepare: Prepare = identityPrepare, extractor: Extractor[T] = identityExtractor)(info: ExecutionInfo, dc: DatasourceContext): Result[RunQueryResult[T]]
+//
+//  def executeQuerySingle[T](sql: String, prepare: Prepare = identityPrepare, extractor: Extractor[T] = identityExtractor)(info: ExecutionInfo, dc: DatasourceContext): Result[RunQuerySingleResult[T]]
 }
