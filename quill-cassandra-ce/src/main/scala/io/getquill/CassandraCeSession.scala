40ca2b31f9511f4ee9ddf3bb43a2d69aa16278e8
voropaevp <voropaev.p@gmail.com>
2021-07-04 15:02:42 +0100

    before merging sbt fixes

--- quill-cassandra-ce/src/main/scala/io/getquill/CassandraCeSession.scala
+++ quill-cassandra-ce/src/main/scala/io/getquill/CassandraCeSession.scala
@@ -1,9 +1,9 @@
 package io.getquill
 
-import com.datastax.driver.core.{BoundStatement, Cluster, PreparedStatement}
+import com.datastax.driver.core.{ BoundStatement, Cluster, PreparedStatement }
 import com.typesafe.config.Config
 import io.getquill.context.cassandra.PrepareStatementCache
-import io.getquill.context.{AsyncFutureCache, CassandraSession, SyncCache}
+import io.getquill.context.{ AsyncFutureCache, CassandraSession, SyncCache }
 import io.getquill.util.LoadConfig
 import io.getquill.util.ZioConversions._
 import cats.effect._
@@ -12,21 +12,19 @@ import cats._
 import cats.implicits._
 import zio.blocking.Blocking
 
-
 case class CassandraCeSession(
-                               override val cluster: Cluster,
-                               override val keyspace: String,
-                               override val preparedStatementCacheSize: Long
-                             ) extends CassandraSession with SyncCache with AsyncFutureCache with AutoCloseable
+  override val cluster:                    Cluster,
+  override val keyspace:                   String,
+  override val preparedStatementCacheSize: Long
+) extends CassandraSession with SyncCache with AsyncFutureCache with AutoCloseable
 
+object CassandraCeSession {
+  def fromContextConfig[F[_]: Sync](config: CassandraContextConfig)(implicit val sf: Sync) = Resource.fromAutoCloseable(
+    sf.defer(CassandraZioSession(config.cluster, config.keyspace, config.preparedStatementCacheSize))
+  )
 
+  def fromConfig[F[_]: Sync](config: Config) = fromContextConfig(CassandraContextConfig(config))
 
-object CassandraCeSession{
-  def fromContextConfig[F[_] : Sync](config: CassandraContextConfig)(implicit  val sf: Sync) = Resource.fromAutoCloseable(
-    sf.defer(CassandraZioSession(config.cluster, config.keyspace, config.preparedStatementCacheSize)))
-
-  def fromConfig[F[_] : Sync](config: Config) = fromContextConfig(CassandraContextConfig(config))
-
-  def fromPrefix[F[_] : Sync](configPrefix: String) = fromContextConfig(CassandraContextConfig(LoadConfig(configPrefix)))
+  def fromPrefix[F[_]: Sync](configPrefix: String) = fromContextConfig(CassandraContextConfig(LoadConfig(configPrefix)))
 
 }
